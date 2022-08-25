df6716d1abd23dbcb8ee93bf550311971cbec220
Vitor Martins <vitormartinsxd@outlook.com>
2022-08-25 12:46:52 -0300

    Finalização do Blog Pessoal em React.

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <div className="barra">
+        <h2 className="titulo">BlogPessoal</h2>
+        <h3 className="titulo">Home</h3>
+        <h3 className="titulo">Postagens</h3>
+        <h3 className="titulo">Temas</h3>
+        <h3 className="titulo">Cadastrar Tema</h3>
+        <h3 className="titulo">Sair</h3>
+        <img className="img" src="https://i.imgur.com/H88yIo2.png" alt="Imagem" />
+      </div>
+
+    </>
+  );
+}
+
+export default Home;
