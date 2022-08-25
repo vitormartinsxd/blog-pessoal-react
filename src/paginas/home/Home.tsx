bb48194bd6f703d7e5d84588dbd61661860b2d4b
Vitor Martins <vitormartinsxd@outlook.com>
2022-08-25 14:00:13 -0300

    Blog Pessoal em React.

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -5,13 +5,27 @@ function Home() {
   return (
     <>
       <div className="barra">
+        <a href="">
         <h2 className="titulo">BlogPessoal</h2>
-        <h3 className="titulo">Home</h3>
-        <h3 className="titulo">Postagens</h3>
-        <h3 className="titulo">Temas</h3>
-        <h3 className="titulo">Cadastrar Tema</h3>
-        <h3 className="titulo">Sair</h3>
+        </a>
+        <a href="">
+        <h4 className="titulo">Home</h4>
+        </a>
+        <a href="">
+        <h4 className="titulo">Postagens</h4>
+        </a>
+        <a href="">
+        <h4 className="titulo">Temas</h4>
+        </a>
+        <a href="">
+        <h4 className="titulo">Cadastrar Tema</h4>
+        </a>
+        <a href="">
+        <h4 className="titulo">Sair</h4>
+        </a>
         <img className="img" src="https://i.imgur.com/H88yIo2.png" alt="Imagem" />
+        <h4 className="sub">Todas as postagens</h4>
+        <h4 className="sub">Sobre nós</h4>
       </div>
 
     </>
