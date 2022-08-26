345b0fe4aa02e0050633bbeef06d7ee1da6a6325
Vitor Martins <vitormartinsxd@outlook.com>
2022-08-26 14:49:27 -0300

    Adicionado Rodapé, Home e Footer na pagina principal.

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,35 +1,32 @@
-import React from "react";
-import "./Home.css";
+import React from 'react';
+import {Typography, Grid, Button} from '@material-ui/core';
+import './Home.css';
+import { Box } from '@mui/material';
+
 
 function Home() {
-  return (
-    <>
-      <div className="barra">
-        <a href="">
-        <h2 className="titulo">BlogPessoal</h2>
-        </a>
-        <a href="">
-        <h4 className="titulo">Home</h4>
-        </a>
-        <a href="">
-        <h4 className="titulo">Postagens</h4>
-        </a>
-        <a href="">
-        <h4 className="titulo">Temas</h4>
-        </a>
-        <a href="">
-        <h4 className="titulo">Cadastrar Tema</h4>
-        </a>
-        <a href="">
-        <h4 className="titulo">Sair</h4>
-        </a>
-        <img className="img" src="https://i.imgur.com/H88yIo2.png" alt="Imagem" />
-        <h4 className="sub">Todas as postagens</h4>
-        <h4 className="sub">Sobre nós</h4>
-      </div>
-
-    </>
-  );
+    return (
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#2F4F4F" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h6" gutterBottom color="textPrimary" component="h6" align="center" style={{ color: "white", fontWeight: "bold" }}>Blog focado em futebol, <br></br> Digite aqui!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#2F4F4F", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
+        </>
+    );
 }
 
 export default Home;
